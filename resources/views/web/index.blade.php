da7bded129534945f710efa047e7a7504087c518
klvst3r <klvst3r@gmail.com>
2018-06-21 14:03:55 -0500

    Add new variables with asset

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -278,13 +278,13 @@
 
     <?php  //include "php/script.php"?>
     <script src="{{asset('public/front/js/jquery.min.js')}}"></script>
-<script src="public/front/js/bootstrap.min.js"></script>
-<script src="public/front/js/owl.carousel.js"></script>
-<script src="public/front/js/jquery.bootpag.min.js"></script>
-<script src="public/front/js/code.js"></script>
-<script src="public/front/shadowbox/shadowbox.js"></script>
+<script src="{{asset('public/front/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('public/front/js/owl.carousel.js')}}"></script>
+<script src="{{asset('public/front/js/jquery.bootpag.min.js')}}"></script>
+<script src="{{asset('public/front/js/code.js')}}"></script>
+<script src="{{asset('public/front/shadowbox/shadowbox.js')}}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCY7Y3OlgCus_BTPAGnAuCUGrBs72saGDk"></script>
-<script src="public/front/js/gmaps.js"></script>
+<script src="{{asset('public/front/js/gmaps.js')}}"></script>
 <script type="text/javascript">
         Shadowbox.init();
 </script>
